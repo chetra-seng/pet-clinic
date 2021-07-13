039b078a1c7b1c408c728c219c7e3533a8ab0442
chetra_seng <sengchetra.love@icloud.com>
2021-07-13 23:43:44 +0700

    Initialize Spring

---
+++ src/test/java/meow/springframe/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package meow.springframe.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
