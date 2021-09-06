b42da8c916b869df2e79e1803336ae756bd5d0d6
chetra_seng <sengchetra.love@icloud.com>
2021-09-06 22:28:29 +0700

    Added Vet page and Controller. Closes #11

---
+++ pet-clinic-web/src/main/java/meow/springframework/petclinic/controllers/VetController.java
@@ -0,0 +1,12 @@
+package meow.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+    @RequestMapping({"/vets", "vets/index"})
+    String vetIndex(){
+        return "vets/vet-index";
+    }
+}
