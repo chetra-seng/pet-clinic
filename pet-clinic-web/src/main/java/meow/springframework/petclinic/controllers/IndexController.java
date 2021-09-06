8a6ff980b81b9abc962b18cfa4312433c9928525
chetra_seng <sengchetra.love@icloud.com>
2021-09-06 21:12:23 +0700

    Added index page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/meow/springframework/petclinic/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package meow.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "/index"})
+    public String index(){
+        return "index";
+    }
+}
