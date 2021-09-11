cd4b33ff52b7354b619334de83f42564e6813570
chetra_seng <sengchetra.love@icloud.com>
2021-09-11 19:54:57 +0700

    List all vets. Closes #14

--- pet-clinic-web/src/main/java/meow/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/meow/springframework/petclinic/bootstrap/DataLoader.java
@@ -40,7 +40,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(1L);
+        vet2.setId(2L);
         vet2.setLastName("Vose");
         vet2.setFirstName("Emma");
         vetService.save(vet2);
--- pet-clinic-web/src/main/java/meow/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/meow/springframework/petclinic/controllers/VetController.java
@@ -1,12 +1,21 @@
 package meow.springframework.petclinic.controllers;
 
+import meow.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "vets/index"})
-    String vetIndex(){
+    String vetIndex(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/vet-index";
     }
 }
