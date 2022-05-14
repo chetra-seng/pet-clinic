4d9d345f6702bd2077f5cedfab85ac58254a4586
chetra_seng <sengchetra.love@icloud.com>
2022-05-14 22:14:49 +0700

    Convert Vets to entity. Closes #37

--- pet-clinic-data/src/main/java/meow/springframework/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/meow/springframework/petclinic/model/Specialty.java
@@ -1,6 +1,13 @@
 package meow.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Specialty extends BaseEntity{
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/meow/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/meow/springframework/petclinic/model/Vet.java
@@ -1,9 +1,14 @@
 package meow.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
